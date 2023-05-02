4cd371cdc3a21a9b850f2b0818b85e2ff9bb1c2e
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-05-02 10:47:24 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    trace: {
+      serviceName: 'vercel-testing-service'
+    },
+  },
 }
 
 module.exports = nextConfig
