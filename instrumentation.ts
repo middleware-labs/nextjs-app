a1f596ac17c20536a4ea27d127cc5fa0480d264c
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-05-03 11:36:45 +0530

    changes

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,7 @@
 // instrumentation.ts
 // import { registerOTel } from '@vercel/otel'
 import { NodeSDK } from '@opentelemetry/sdk-node'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { Resource } from '@opentelemetry/resources'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node'
@@ -11,7 +11,7 @@ export function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
         const sdk = new NodeSDK({
             resource: new Resource({
-                [SemanticResourceAttributes.SERVICE_NAME]: 'next-app',
+                [SemanticResourceAttributes.SERVICE_NAME]: 'tested-next-app-test122',
             }),
             spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
         })
