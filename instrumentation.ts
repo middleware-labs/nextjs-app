e4f3742655a5450b86e9ca0603e91e90531363ae
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-05-03 16:35:36 +0530

    changes

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,11 +1,60 @@
 // instrumentation.ts
 // @ts-ignore
-import { track } from '@middleware.io/agent-apm-nextjs';
+// import { track } from '@middleware.io/agent-apm-nextjs';
+import {SemanticResourceAttributes} from "@opentelemetry/semantic-conventions";
+import {NodeTracerProvider, SimpleSpanProcessor} from "@opentelemetry/sdk-trace-node";
+import {Resource} from "@opentelemetry/resources";
+import {OTLPTraceExporter} from "@opentelemetry/exporter-trace-otlp-http";
+import process from 'process';
 
 export function register() {
-    track({
+    /*track({
         projectName: "vercel-mw-test-project",
         serviceName: "vercel-mw-test-service",
+        target: "vercel",
+    });*/
+
+    if (process.env.NEXT_RUNTIME !== 'nodejs') {
+        return;
+    }
+
+    const args = {
         target: "vercel"
+    }
+
+    const config = {
+        hostUrl: 'http://localhost:9320',
+        projectName: "Project-" + process.pid,
+        serviceName: "Service-" + process.pid,
+        accountKey: "",
+        target: "",
+        ...args,
+    };
+
+    const resourceAttributes = {
+        [SemanticResourceAttributes.SERVICE_NAME]: config.serviceName,
+        ['mw_agent']: true,
+        ['project.name']: config.projectName,
+        ...(config.accountKey !== "" && {'mw.account_key': config.accountKey}),
+    };
+
+    if (config.target !== "") {
+        config.hostUrl = config.target;
+    }
+
+    if (!!(process.env.MW_AGENT_SERVICE && process.env.MW_AGENT_SERVICE !== "")) {
+        config.hostUrl = `http://${process.env.MW_AGENT_SERVICE}:9320`;
+    }
+
+    const configUrl = ((config.target).toLowerCase() === "vercel") ? {} : {url: `${config.hostUrl}/v1/traces`};
+
+    const provider = new NodeTracerProvider({
+        resource: new Resource(resourceAttributes),
     });
+
+    provider.register();
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter(configUrl)));
+
+    return provider;
 }
