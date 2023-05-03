d73d2d70db3945bfd50a5a54099f5cd4f1c4a2e3
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-05-03 18:28:14 +0530

    changes

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,7 @@
 // instrumentation.ts
 // @ts-ignore
-import { track } from '@middleware.io/agent-apm-nextjs';
+// import { track } from '@middleware.io/agent-apm-nextjs';
+import { registerOTel } from '@vercel/otel'
 /*import {SemanticResourceAttributes} from "@opentelemetry/semantic-conventions";
 import {NodeTracerProvider, SimpleSpanProcessor} from "@opentelemetry/sdk-trace-node";
 import {Resource} from "@opentelemetry/resources";
@@ -8,11 +9,14 @@ import {OTLPTraceExporter} from "@opentelemetry/exporter-trace-otlp-http";
 import process from 'process';*/
 
 export function register() {
-    return track({
+
+    registerOTel('next-app')
+
+    /*return track({
         projectName: "vercel-mw-test-project",
         serviceName: "vercel-mw-test-service",
         target: "vercel",
-    });
+    });*/
 
     /*if (process.env.NEXT_RUNTIME !== 'nodejs') {
         return;
