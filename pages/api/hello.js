7dc45908fe9b723806073acfeb37cc1a02df9b1d
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-05-03 19:00:32 +0530

    changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,17 +5,17 @@ const tracer = trace.getTracer("next-app-tracer");
 */
 
 export default async function handler(req, res) {
-    /*const span = tracer.startSpan("helloHandler", {
+    const span = tracer.startSpan("helloHandler", {
         attributes: {
             "http.method": req.method,
             "http.url": req.url,
         },
-    }, context.active());*/
+    }, context.active());
     const ms = Math.floor(Math.random() * 1000);
     // span.setAttribute("sleep", ms);
     // await new Promise((resolve) => setTimeout(resolve, ms));
 
     res.status(200).json({ greetings: `Hi there ${ms}!` });
 
-    // span.end();
+    span.end();
 }
