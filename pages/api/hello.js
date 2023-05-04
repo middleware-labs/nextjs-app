47c4b00cd1b720e5e83b42c39e18a91e2fb12e9c
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-05-04 16:15:08 +0530

    changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,17 +3,17 @@ import { trace, context } from '@opentelemetry/api'
 const tracer = trace.getTracer("next-app-tracer");
 
 export default async function handler(req, res) {
-    const span = tracer.startSpan("helloHandler", {
+    /*const span = tracer.startSpan("helloHandler", {
         attributes: {
             "http.method": req.method,
             "http.url": req.url,
         },
     }, context.active());
-    const ms = Math.floor(Math.random() * 1000);
+    const ms = Math.floor(Math.random() * 1000);*/
     // span.setAttribute("sleep", ms);
     // await new Promise((resolve) => setTimeout(resolve, ms));
 
     res.status(200).json({ greetings: `Hello There.. ${ms}` });
 
-    span.end();
+    // span.end();
 }
