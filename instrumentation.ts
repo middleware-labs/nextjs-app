774166dcd1e7b0bbd1292628148924eee451fe64
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-06-16 16:14:00 +0530

    minor

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,13 +3,6 @@ import tracker from '@middleware.io/agent-apm-nextjs';
 
 export function register() {
 
-    /*tracker.track({
-        projectName: "vercel-mw-test-project",
-        serviceName: "vercel-mw-test-service",
-        target: "https://lveha.front.env.middleware.io:443",
-        accountKey: "kyrqzxybzmqfoaoqwclrposbacczzfgztpmk"
-    });*/
-
     tracker.track({
         projectName: "vercel-project-1000",
         serviceName: "vercel-service-1000",
@@ -17,5 +10,12 @@ export function register() {
         target: "vercel",
     });
 
-    tracker.info("ONE TIME INSTRUMENTATION DONE");
+    /*tracker.track({
+        projectName: "ABC-1",
+        serviceName: "ABC-2",
+        accountKey: "xusuusalpvush63ud7zcg8bi3mauuptds528",
+        target: "https://p2i13hg.middleware.io:443",
+    });*/
+
+    tracker.info("ONE TIME INSTRUMENTATION DONE with SECOND TRY..");
 }
