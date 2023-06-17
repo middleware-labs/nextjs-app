4295ed892eb064854b08b21c998c1c9ea5591fa1
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-06-17 17:51:26 +0530

    setup 10th

--- instrumentation.ts
+++ instrumentation.ts
@@ -17,5 +17,5 @@ export function register() {
         target: "https://p2i13hg.middleware.io:443",
     });*/
 
-    tracker.info("ONE TIME INSTRUMENTATION DONE with Third TRY..");
+    tracker.info("toto ONE TIME INSTRUMENTATION DONE with Third TRY..");
 }
