cf93c6c4ddbb9d5aa637b545230ae9c7969b9cfd
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-07-26 18:10:43 +0530

    commit 102

--- instrumentation.ts
+++ instrumentation.ts
@@ -5,17 +5,28 @@ import tracker from '@middleware.io/agent-apm-nextjs';
 export function register() {
 
     tracker.track({
-        projectName: "vercel-project-1000",
-        serviceName: "vercel-service-1000",
-        accountKey: "xusuusalpvush63ud7zcg8bi3mauuptds528",
+        projectName: "vercel-project-61",
+        serviceName: "vercel-service-62",
+        accessToken: "xusuusalpvush63ud7zcg8bi3mauuptds528",
         target: "vercel",
     });
 
     /*tracker.track({
-        projectName: "ABCxyz-1004",
-        serviceName: "ABCxyz-2004",
+        projectName: "ABCxyz-1008",
+        serviceName: "ABCxyz-2008",
         accountKey: "xusuusalpvush63ud7zcg8bi3mauuptds528",
         target: "https://p2i13hg.middleware.io:443",
+    });*/
+
+    // https://app.stage.env.middleware.io/api/v1/auth
+    // https://profiling.stage.env.middleware.io
+    // MW_AUTH_URL=https://app.stage.env.middleware.io/api/v1/auth MW_PROFILING_SERVER_URL=https://profiling.stage.env.middleware.io  npm run dev
+
+    /*tracker.track({
+        projectName: "profiling-matters-51",
+        serviceName: "profiling-matters-52",
+        accessToken: "deoorojzrgpsvihfcgkcsvhrwnmzqeahvhou",
+        target: "https://deofs.stage.env.middleware.io:443",
     });*/
 
     /*tracker.track({
@@ -23,5 +34,5 @@ export function register() {
         serviceName:"Node1-2001",
     })*/
 
-    tracker.info("Twooo222211 TIME INSTRUMENTATION DONE with SECOND TRY..");
+    tracker.info("Profiling matter continues...123");
 }
